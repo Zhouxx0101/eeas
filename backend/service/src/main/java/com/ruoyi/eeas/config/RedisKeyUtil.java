176355f4ac8ea6cf82f2ef231c0958e9f9e14cf8
Zhou Xinxing <1811526@qq.com>
2023-03-21 16:13:55 +0800

    feat: 时序交互图

---
+++ backend/service/src/main/java/com/ruoyi/eeas/config/RedisKeyUtil.java
@@ -0,0 +1,6 @@
+package com.ruoyi.eeas.config;
+
+public class RedisKeyUtil {
+    public static final String REDIS_PLACE_LONGITUDE_LATITUDE = "eeas::place_longitude_latitude_";
+    public static final Integer REDIS_PLACE_LONGITUDE_LATITUDE_EXPIRATION = 2;
+}
